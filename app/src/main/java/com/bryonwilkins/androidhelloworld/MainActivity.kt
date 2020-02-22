eedb7637b4b0f2ae1377bc2fafb27822b16b0b39
paradime <brw4824@rit.edu>
2020-02-21 20:58:31 -0500

    initial commit!

---
+++ app/src/main/java/com/bryonwilkins/androidhelloworld/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.bryonwilkins.androidhelloworld
+
+import android.os.Bundle
+import android.util.Log
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+
+        Toast.makeText(this, "Can you see me?", Toast.LENGTH_SHORT).show()
+
+        Log.i("info", "Done creating the app.")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    fun topClick(v: View) {
+        Toast.makeText(this, "Can you see me?", Toast.LENGTH_SHORT).show()
+    }
+
+    fun bottomClick(v: View) {
+        Toast.makeText(this, "It's me... Pengi!", Toast.LENGTH_SHORT).show()
+    }
+}
